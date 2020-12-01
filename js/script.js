c30bcd791a4f6608a452a4e504f4ec4558e73c50
Luca Cavretti <cavretiluca@live.it>
2020-12-01 17:07:29 +0100

    Trying to render albums titles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
+const myAlbums = [];
+
 const myApp = new Vue({
     el: '#root',
-    data: {},
+    data: {
+        albums: [...myAlbums]
+    },
     method: {},
-
+    mounted() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(r => {
+                myAlbums.push(r.data.response);
+            })
+    }
 })
