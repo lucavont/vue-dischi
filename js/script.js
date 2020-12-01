def3f96cf6c64bf19a141f2a5c31248277af32c7
Luca Cavretti <cavretiluca@live.it>
2020-12-01 18:14:12 +0100

    Finished exercise

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-const myAlbums = [];
-
 const myApp = new Vue({
     el: '#root',
     data: {
-        albums: [...myAlbums]
+        albums: []
     },
     method: {},
     mounted() {
         axios
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(r => {
-                myAlbums.push(r.data.response);
+                this.albums = r.data.response;
             })
     }
 })
